c4b52a952e7c62fdd6c9122722daca9f64db0cf6
Dove-white <dlsdove@gmail.com>
2024-06-17 23:32:17 +0000

    Feat: vite setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center font-bold text-2xl text-red-400">
+      am expense app
+    </div>
+  );
+};
+
+export default App;
